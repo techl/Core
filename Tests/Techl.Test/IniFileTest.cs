6b45e15a6d485807ab204e7a0422f454b963beb3
Bongho Lee <bhlee@sudo.co.kr>
2017-09-12 23:23:35 +0900

    Add IniFile test code

--- Tests/Techl.Test/IniFileTest.cs
+++ Tests/Techl.Test/IniFileTest.cs
@@ -7,21 +7,38 @@ namespace Techl.Test
     [TestClass]
     public class IniFileTest
     {
-        [TestMethod]
-        public void IniFileTest()
+        IniFile File;
+
+        [TestInitialize]
+        public void Initialize()
         {
-            var file = new IniFile(DirectoryHelper.GetBaseDirectory() + @"\..\..\Init.ini");
-            var value = file.Read("Section1", "A", "");
+            File = new IniFile(DirectoryHelper.GetBaseDirectory() + @"\..\..\Init.ini");
+
+        }
+
+        [TestMethod]
+        public void IniFileOverallTest()
+        {
+            var value = File.Read("Section1", "A", "");
             Assert.IsTrue(value == "3");
 
-            var value2 = file.Read("Section1", "B", 0);
+            var value2 = File.Read("Section1", "B", 0);
             Assert.IsTrue(value2 == 4);
 
-            var value3 = file.Read("Section2", "A", 0f);
+            var value3 = File.Read("Section2", "A", 0f);
             Assert.IsTrue(value3 == 0f);
 
-            var value4 = file.Read("Section2", "C", 0f);
+            var value4 = File.Read("Section2", "C", 0f);
             Assert.IsTrue(value4 == 5.5f);
+
+            File.DeleteKey("Section1", "A");
+
+        }
+
+        [TestMethod]
+        public void DeleteSectionTest()
+        {
+            File.DeleteSection("Section1");
         }
     }
 }
