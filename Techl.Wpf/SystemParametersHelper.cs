eca1a23f549f6a510b8a4a772e4da7f54b3e8519
Bongho Lee <bhlee@sudo.co.kr>
2018-04-04 13:11:40 +0900

    Add Techl.Wpf

---
+++ Techl.Wpf/SystemParametersHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Techl.Wpf
+{
+    public class SystemParametersHelper
+    {
+        public static bool MenuDropAlignment
+        {
+            get
+            {
+                return SystemParameters.MenuDropAlignment;
+            }
+            set
+            {
+                var _menuDropAlignmentField = typeof(SystemParameters).GetField("_menuDropAlignment", BindingFlags.NonPublic | BindingFlags.Static);
+                _menuDropAlignmentField?.SetValue(null, value);
+            }
+        }
+    }
+}
---
+++ Techl.Wpf/VersionHistory.md
@@ -0,0 +1,4 @@
+﻿# Version History
+
+v1.0.0
+    Add SystemParametersHelper.MenuDropAlignment
