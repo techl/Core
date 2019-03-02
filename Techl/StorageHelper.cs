b8e05821b5379e2b6dee7538ae37f182a241f251
Bongho Lee <bhlee@sudo.co.kr>
2019-03-02 22:33:42 +0900

    Restore StorageHelper

---
+++ Techl/StorageHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Techl
+{
+    public static class StorageHelper
+    {
+        public static string LocalPath
+        {
+            get
+            {
+                return AppContext.BaseDirectory;
+            }
+        }
+    }
+}
---
+++ Tests/Techl.Test/StorageHelperTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Techl.Test
+{
+    [TestClass]
+    public class StorageHelperTest
+    {
+        [TestMethod]
+        public void LocalPathTest()
+        {
+            Debug.WriteLine(StorageHelper.LocalPath);
+        }
+    }
+}
