a68ca4b29e2d4a5c82a5abcce774665d0b2b4708
Bongho Lee <bhlee@sudo.co.kr>
2020-11-17 17:05:07 +0900

    Add WriteInfo

--- Techl/Diagnostics/Log.cs
+++ Techl/Diagnostics/Log.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using System.Diagnostics;
+using Techl.Reflection;
 
 namespace Techl.Diagnostics
 {
@@ -24,22 +25,34 @@ namespace Techl.Diagnostics
 
         public static void Write(string category, string message)
         {
+            string fileName = GetFileName(category);
+            string content = $"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}][{category}] {message}";
+
+            WriteInternal(fileName, content);
+        }
+
+        public static void WriteInfo(string message)
+        {
+            var fileName = Path.GetFileNameWithoutExtension(AssemblyHelper.GetEntryAssemblyFilePath()) + ".log";
+            string content = $"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}] {message}";
+
+            WriteInternal(fileName, content);
+        }
+
+        private static void WriteInternal(string fileName, string content)
+        {
             try
             {
-                string filename = GetFileName(category);
-
-                string content = $"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}][{category}] {message}";
-
-                if (!File.Exists(filename))
+                if (!File.Exists(fileName))
                 {
-                    using (StreamWriter sw = File.CreateText(filename))
+                    using (StreamWriter sw = File.CreateText(fileName))
                     {
                         sw.WriteLine(content);
                     }
                 }
                 else
                 {
-                    using (StreamWriter sw = File.AppendText(filename))
+                    using (StreamWriter sw = File.AppendText(fileName))
                     {
                         sw.WriteLine(content);
                     }
@@ -50,6 +63,36 @@ namespace Techl.Diagnostics
             catch (Exception ex)
             {
                 Trace.WriteLine($"Log.Write Exception : {ex}");
+
+                var exContent = ex.ToString() + $"\nContent={content}";
+                var errorFileName = GetFileName("Log.Write.Error");
+                
+                for (int i = 0; i < 3; i++)
+                {
+                    try
+                    {
+                        if (!File.Exists(errorFileName))
+                        {
+                            using (StreamWriter sw = File.CreateText(errorFileName))
+                            {
+                                sw.WriteLine(exContent);
+                            }
+                        }
+                        else
+                        {
+                            using (StreamWriter sw = File.AppendText(errorFileName))
+                            {
+                                sw.WriteLine(exContent);
+                            }
+                        }
+
+                        return;
+                    }
+                    catch
+                    {
+                        System.Threading.Thread.Sleep(100);
+                    }
+                }
             }
         }
     }
